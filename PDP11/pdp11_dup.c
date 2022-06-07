open-simh/simh

commit b41acb30c191e39999546782b68fc4cd8070fb35

PDP11, Qbus & Unibus VAX: Extend DUP device for DECnet Phase V support
- This involves adding some trailing characters to received DDCMP frames
  to meet host driver expectations (simulation then behaves more like a
  real synchronous device). 1 SYN is enough for VMS, but RSX needs a few
  when there is no abutting frame.
- The VMS driver implements the CRC performance option in section 5.1.2
  of the DDCMP spec., and thus relies on receiving a valid character after
  the CRC on a received frame.

PDP11/pdp11_dup.c
@@ -36,6 +36,7 @@
    characters both initially and between DDCMP packets.
 
    15-May-13    MP      Initial implementation
+
 */
 
 #if defined (VM_PDP10)                                  /* PDP10 version */
@@ -316,7 +317,8 @@ static BITFIELD dup_txdbuf_bits[] = {
 #define TXDBUF_MBZ ((1<<15)|(1<<13))
 #define TXDBUF_WRITEABLE (TXDBUF_M_TABRT|TXDBUF_M_TEOM|TXDBUF_M_TSOM|TXDBUF_M_TXDBUF)
 
-
+#define TRAILING_SYNS 8
+const uint8 tsyns[TRAILING_SYNS] = {0x96,0x96,0x96,0x96,0x96,0x96,0x96,0x96}; 
 
 /* DUP data structures
 
@@ -576,7 +578,7 @@ switch ((PA >> 1) & 03) {                               /* case on PA<2:1> */
         if ((!(dup_rxcsr[dup] & RXCSR_M_RCVEN)) && 
             (orig_val & RXCSR_M_RCVEN)) {               /* Downward transition of receiver enable */
             dup_rxdbuf[dup] &= ~RXDBUF_M_RXDBUF;
-            dup_rxcsr[dup] &= ~RXCSR_M_RXACT;
+            dup_rxcsr[dup] &= ~(RXCSR_M_RXACT|RXCSR_M_RXDONE);    /* also clear RXDONE per DUP11 spec. */
             if ((dup_rcvpkinoff[dup] != 0) || 
                 (dup_rcvpkbytes[dup] != 0))
                 dup_rcvpkinoff[dup] = dup_rcvpkbytes[dup] = 0;
@@ -967,8 +969,8 @@ dup_rxdbuf[dup] &= ~RXDBUF_M_RCRCER;
 dup_rxdbuf[dup] &= ~RXDBUF_M_RXDBUF;
 dup_rxdbuf[dup] |= dup_rcvpacket[dup][dup_rcvpkinoff[dup]++];
 dup_rxcsr[dup] |= RXCSR_M_RXDONE;
-if (((dup_rcvpkinoff[dup] == 8) || 
-     (dup_rcvpkinoff[dup] >= dup_rcvpkbytes[dup])) &&
+if ( ((dup_rcvpkinoff[dup] == 8) || 
+      (dup_rcvpkinoff[dup] >= dup_rcvpkbytes[dup]-TRAILING_SYNS)) && /* don't include trailing SYNs in CRC calc */ 
     (0 == ddcmp_crc16 (0, dup_rcvpacket[dup], dup_rcvpkinoff[dup])))
     dup_rxdbuf[dup] |= RXDBUF_M_RCRCER;
 else
@@ -1070,13 +1072,19 @@ for (dup=active=attached=0; dup < dup_desc.lines; dup++) {
             r = tmxr_get_packet_ln (lp, &buf, &size_t_size);
             size = (uint16)size_t_size;
             }
+        /* in DEC mode add some SYN bytes to the end to deal with host drivers that 
+           implement the DDCMP CRC performance optimisation (DDCMP V4.0 section 5.1.2) */
         if ((r == SCPE_OK) && (buf)) {
-            if (dup_rcvpksize[dup] < size) {
-                dup_rcvpksize[dup] = size;
+            if (dup_rcvpksize[dup] < size + TRAILING_SYNS) {
+                dup_rcvpksize[dup] = size + TRAILING_SYNS;
                 dup_rcvpacket[dup] = (uint8 *)realloc (dup_rcvpacket[dup], dup_rcvpksize[dup]);
                 }
             memcpy (dup_rcvpacket[dup], buf, size);
             dup_rcvpkbytes[dup] = size;
+            if (dup_parcsr[dup] & PARCSR_M_DECMODE) {
+                memcpy(&(dup_rcvpacket[dup][size]), tsyns, TRAILING_SYNS);
+                dup_rcvpkbytes[dup] += TRAILING_SYNS ;
+            }
             dup_rcvpkinoff[dup] = 0;
             dup_rxcsr[dup] |= RXCSR_M_RXACT;
             dup_rcv_byte (dup);

commit 6e0e5a714d9fa48a533baf8c89bfd7e01fc45bf8

PDP11 & Unibus VAX, PDP10: Fix DUP device support for use by internal KMC for TOPS-20
This fixes a regression caused by commit b41acb3

PDP11/pdp11_dup.c
@@ -74,6 +74,7 @@ static uint16 dup_txdbuf[DUP_LINES];
 static t_bool dup_W3[DUP_LINES];
 static t_bool dup_W5[DUP_LINES];
 static t_bool dup_W6[DUP_LINES];
+static t_bool dup_kmc[DUP_LINES];                       /* being used by a kmc or other internal simulator device */
 static uint32 dup_rxi = 0;                                     /* rcv interrupts */
 static uint32 dup_txi = 0;                                     /* xmt interrupts */
 static uint32 dup_wait[DUP_LINES];                             /* rcv/xmt byte delay */
@@ -837,6 +838,7 @@ if (!protocol_DDCMP) {
 if (crc_inhibit) {
     return SCPE_ARG;                /* Must enable CRC for DDCMP */
     }
+dup_kmc[dup] = TRUE;     /* remember we are being used by an internal simulator device */
 /* These settings reflect how RSX operates a bare DUP when used for 
    DECnet communications */
 dup_clear(dup, FALSE);
@@ -955,6 +957,8 @@ return SCPE_OK;
 
 static t_stat dup_rcv_byte (int32 dup)
 {
+int32 crcoffset;
+    
 sim_debug (DBG_TRC, DUPDPTR, "dup_rcv_byte(dup=%d) - %s, byte %d of %d\n", dup, 
            (dup_rxcsr[dup] & RXCSR_M_RCVEN) ? "enabled" : "disabled",
            dup_rcvpkinoff[dup], dup_rcvpkbytes[dup]);
@@ -966,13 +970,15 @@ if (dup_rcv_packet_data_callback[dup]) {
     dup_rcv_packet_data_callback[dup](dup, dup_rcvpkbytes[dup]);
     return SCPE_OK;
     }
+/* if we added trailing SYNs, don't include them in the CRC calc */
+crcoffset = (dup_kmc[dup] ? 0 : TRAILING_SYNS);
 dup_rxcsr[dup] |= RXCSR_M_RXACT;
 dup_rxdbuf[dup] &= ~RXDBUF_M_RCRCER;
 dup_rxdbuf[dup] &= ~RXDBUF_M_RXDBUF;
 dup_rxdbuf[dup] |= dup_rcvpacket[dup][dup_rcvpkinoff[dup]++];
 dup_rxcsr[dup] |= RXCSR_M_RXDONE;
 if ( ((dup_rcvpkinoff[dup] == 8) || 
-      (dup_rcvpkinoff[dup] >= dup_rcvpkbytes[dup]-TRAILING_SYNS)) && /* don't include trailing SYNs in CRC calc */ 
+      (dup_rcvpkinoff[dup] >= dup_rcvpkbytes[dup]-crcoffset)) && 
     (0 == ddcmp_crc16 (0, dup_rcvpacket[dup], dup_rcvpkinoff[dup])))
     dup_rxdbuf[dup] |= RXDBUF_M_RCRCER;
 else
@@ -1083,7 +1089,7 @@ for (dup=active=attached=0; dup < dup_desc.lines; dup++) {
                 }
             memcpy (dup_rcvpacket[dup], buf, size);
             dup_rcvpkbytes[dup] = size;
-            if (dup_parcsr[dup] & PARCSR_M_DECMODE) {
+            if (!dup_kmc[dup] && (dup_parcsr[dup] & PARCSR_M_DECMODE)) {
                 memcpy(&(dup_rcvpacket[dup][size]), tsyns, TRAILING_SYNS);
                 dup_rcvpkbytes[dup] += TRAILING_SYNS ;
             }
@@ -1235,11 +1241,12 @@ if (dup_ldsc == NULL) {                                 /* First time startup */
             ++attached;
         }
     dup_units[dup_desc.lines] = dup_poll_unit_template;
-    /* Initialize to standard factory Option Jumper Settings */
+    /* Initialize to standard factory Option Jumper Settings and no associated KMC */
     for (i = 0; i < DUP_LINES; i++) {
         dup_W3[i] = TRUE;
         dup_W5[i] = FALSE;
         dup_W6[i] = TRUE;
+        dup_kmc[i] = FALSE;
         }
     }
 for (i = 0; i < dup_desc.lines; i++) {                  /* init each line */