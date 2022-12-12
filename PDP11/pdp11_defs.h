86a995b863a1662406dd51ba69bb71d4902875cc
Bob Supnik <bob@supnik.org>
2022-12-12 12:00:00 -0500

    PDP11: numerous 11/70 compatibility fixes

--- PDP11/pdp11_defs.h
+++ PDP11/pdp11_defs.h
@@ -26,6 +26,8 @@
    The author gratefully acknowledges the help of Max Burnet, Megan Gentry,
    and John Wilson in resolving questions about the PDP-11
 
+   23-Oct-22    RMS     Moved NXM abort priority above MME trap priority
+   25-Jul-22    RMS     Removed OPT_RH11 (Mark Pizzolato)
    10-Feb-17    RMS     Fixed RJS11 register block length (Mark Hill)
    19-Jan-17    RMS     Moved CR11 to BR6, leaving CD11 at BR4 (Mark Pizzolato)
    10-Mar-16    RMS     Added UC15 support
@@ -409,14 +411,17 @@ typedef struct {
 #define CSR_BUSY        (1u << CSR_V_BUSY)
 #define CSR_ERR         (1u << CSR_V_ERR)
 
-/* Trap masks, descending priority order, following J-11
-   An interrupt summary bit is kept with traps, to minimize overhead
+/* Trap masks, descending priority order. Rules:
+
+   - Aborts are mutually exclusive, no more than one per instrution.
+   - Aborts must be higher priority than traps. Because MME can be
+     either an abort or a trap, it is lower priority than NXM.
 */
 
 #define TRAP_V_RED      0                               /* red stk abort  4 */
 #define TRAP_V_ODD      1                               /* odd address    4 */
-#define TRAP_V_MME      2                               /* mem mgt      250 */
-#define TRAP_V_NXM      3                               /* nx memory      4 */
+#define TRAP_V_NXM      2                               /* nx memory      4 */
+#define TRAP_V_MME      3                               /* mem mgt      250 */
 #define TRAP_V_PAR      4                               /* parity err   114 */
 #define TRAP_V_PRV      5                               /* priv inst      4 */
 #define TRAP_V_ILL      6                               /* illegal inst  10 */
