8c1fb2ea3ad1931ce7f1a3ba94096bec65bb7ad2
Ender Wiggin <96267911+PurpleAce88@users.noreply.github.com>
2024-02-16 14:42:56 +0000

    Update deps.sh
    Changed "arch" to "arch-linux"

--- .travis/deps.sh
+++ .travis/deps.sh
@@ -5,7 +5,7 @@ install_osx() {
     brew install pkg-config pcre libpng libedit sdl2 freetype2 sdl2_ttf \
         vde cmake gnu-getopt coreutils
 }
-install_arch() {
+install_arch-linux() {
     sudo pacman -S --noconfirm pkgconf
     sudo pacman -S --noconfirm pcre libpng libedit
     sudo pacman -S --noconfirm mesa
@@ -65,11 +65,11 @@ install_clang64() {
 
 
 case "$1" in
-  osx|arch|linux|mingw64|ucrt64|clang64)
+  osx|arch-linux|linux|mingw64|ucrt64|clang64)
     install_"$1"
     ;;
   *)
-    echo "$0: Need an operating system name: osx, arch, linux, mingw64 or ucrt64"
+    echo "$0: Need an operating system name: osx, arch-linux, linux, mingw64 or ucrt64"
     exit 1
     ;;
 esac
