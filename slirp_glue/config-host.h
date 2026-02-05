2ce7c403546f8ae2dce5f2af370a07884250f377
Alexey Khromov <zxalexis@gmail.com>
2026-02-05 23:10:17 +0300

    Update config-host.h
    Fix build compatibility with GCC 15.2+ which defaults to -std=c23

--- slirp_glue/config-host.h
+++ slirp_glue/config-host.h
@@ -12,7 +12,9 @@ typedef int SOCKET;
 #endif
 
 #ifndef  __cplusplus
+#if __STDC_VERSION__ < 202311L
 typedef int bool;
+#endif
 #endif
 #ifdef _MSC_VER
 #include <win32/stdint.h>
@@ -54,3 +56,4 @@ int qemu_recv (int s, void *buf, size_t len, int flags);
 #define error_report(...) fprintf(stderr, __VA_ARGS__)
 
 #endif
+
